51a5f499646942f9e43ecd000e378c3e2e9437b9
dimitrimont <montgomerydimitri1@gmail.com>
2025-04-15 15:32:18 -0400

    library driver

--- LibraryManagementSystem/src/main/java/com/library/management/librarymanagementsystem/hello-viewController.java
+++ LibraryManagementSystem/src/main/java/com/library/management/librarymanagementsystem/hello-viewController.java
@@ -3,7 +3,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class PleaseProvideControllerClassName {
+public class hello-viewController {
 
     @FXML
     private TextField authorNameTextField;
