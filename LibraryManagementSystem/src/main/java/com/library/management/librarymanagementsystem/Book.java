f05667441eb6259626c4eb42ebb8843ce9d68c67
Preston Beachum <prestonbeachum@gmail.com>
2025-04-16 13:21:30 -0400

    Few bug changes, GUI now works.

--- LibraryManagementSystem/src/main/java/com/library/management/librarymanagementsystem/Book.java
+++ LibraryManagementSystem/src/main/java/com/library/management/librarymanagementsystem/Book.java
@@ -1,73 +1,36 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+package com.library.management.librarymanagementsystem;
 
 public class Book {
-    private String title;
     private String author;
-    private String genre;
+    private String title;
     private int book_id;
+    private String genre;
 
     public Book(String author, String title, int book_id, String genre) {
-        this.title = title;
         this.author = author;
-        this.genre = genre;
+        this.title = title;
         this.book_id = book_id;
-    }
-
-    public void setBook_id(int book_id) {this.book_id = book_id;}
-
-    public int getBook_id() {return book_id;}
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setGenre(String genre) {
         this.genre = genre;
     }
 
-//    public void setPrice(double price) {
-//        this.price = price;
-//    }
-
-    public void setAuthor(String author) {
-        this.author = author;
+    public String getAuthor() {
+        return author;
     }
 
     public String getTitle() {
         return title;
     }
 
-//    public boolean isCheckedOut() {
-//        return isCheckedOut;
-//    }
-
-//    public String getDateCheckedOut() {
-//        return dateCheckedOut;
-//    }
-
-//    public double getPrice() {
-//        return price;
-//    }
-
-    public String getAuthor() {
-        return author;
+    public int getBook_id() {
+        return book_id;
     }
 
     public String getGenre() {
         return genre;
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public String toString() {
+        return title + " by " + author + " [" + genre + "]";
+    }
 }
